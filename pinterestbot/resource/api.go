90991ec3e2b2706152e0db09c6128ac2b0909280
Zeus Aaron Malvar <zeus.orangeapps@gmail.com>
2020-12-04 10:44:03 +0800

    logging

--- pinterestbot/resource/api.go
+++ pinterestbot/resource/api.go
@@ -2,9 +2,7 @@ package resource
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/dghubble/sling"
@@ -64,15 +62,7 @@ func (a *Api) Get(resource string, request GetParameters, response interface{}) 
 
 	httpResponse, err := a.http.New().Get(resource).QueryStruct(requestParam).Receive(response, err)
 	if err != nil {
-		fmt.Println("[PINTEREST ERROR]: ", err)
-		fmt.Println("[PINTEREST HTTP BODY RESPONSE]: ", &httpResponse.Body)
-
-		body := strings.NewReader("raw body")
-		a, err := a.http.New().Get(resource).Body(body).Request()
-		fmt.Println("[TEST RESOURCE]: ", a)
 		return nil, nil, err
 	}
-	fmt.Println("[PINTEREST HTTP BODY RESPONSE]: ", &httpResponse)
-	fmt.Println("[PINTEREST HTTP RESPONSE]: ", response)
 	return response, httpResponse, nil
 }
