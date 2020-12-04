6ca1d443d6ff62ce90c5c81906702697870b7f25
Zeus Aaron Malvar <zeus.orangeapps@gmail.com>
2020-12-04 09:38:35 +0800

    logging

--- pinterestbot/resource/api.go
+++ pinterestbot/resource/api.go
@@ -62,7 +62,7 @@ func (a *Api) Get(resource string, request GetParameters, response interface{}) 
 	requestParam.Data = string(parsedOptions)
 
 	httpResponse, err := a.http.New().Get(resource).QueryStruct(requestParam).Receive(response, err)
-	fmt.Println("[PINTEREST HTTP RESPONSE]: ", httpResponse)
+	fmt.Println("[PINTEREST HTTP RESPONSE]: ", httpResponse.Body)
 	if err != nil {
 		return nil, nil, err
 	}
