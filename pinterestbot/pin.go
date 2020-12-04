989b70f31013c38769bc6bbfd74d167b2988fc4c
Zeus Aaron Malvar <zeus.orangeapps@gmail.com>
2020-12-04 11:01:55 +0800

    logging

--- pinterestbot/pin.go
+++ pinterestbot/pin.go
@@ -60,7 +60,7 @@ func (s pinService) Search(param SearchParameters) (*pin.Response, error) {
 	fmt.Println("[PINTEREST RESPONSE]: ", string(b))
 
 	if err != nil {
-		return nil, err
+		return response, err
 	}
 
 	return response, nil
